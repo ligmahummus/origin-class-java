e800adafcf128717cad58f2a6ebc82ec8abe3b6c
ligmahummus <xarielah@gmail.com>
2023-11-08 00:13:53 +0200

    feat: batman2

--- src/Origin.java
+++ src/Origin.java
@@ -1,18 +1,16 @@
 import java.util.Scanner;
 
+
 public class Origin {
-    // Constants are flexible if we ever want to check proximity to another coordinates.
-    private static final int X_BASE = 0;
-    private static final int Y_BASE = 0;
-
-    // Declare distance.
-    private static double _distance;
-
-
-
     public static void main(String[] args) {
+        // Constants are flexible if we ever want to check proximity to another coordinates.
+        final int X_BASE = 0;
+        final int Y_BASE = 0;
 
-        // Nearest coordinates uninitialized.
+        // Declare distance.
+        double distance;
+
+        // Declare the nearest coordinates that'll be parsed as the answer at the end.
         int nearest_x;
         int nearest_y;
 
@@ -29,14 +27,14 @@ public class Origin {
 
         // Assigning the first input's calculation since there is nothing initialized
         // to _distance variable.
-        _distance = getDistance(x1, y1);
+        distance = Math.sqrt(Math.pow(x1 - X_BASE, 2) + Math.pow(y1 - Y_BASE, 2));
 
         System.out.println ("Enter second point coordinates:");
         int x2 = scan.nextInt();
         int y2 = scan.nextInt();
 
         // Using math equation to calculate if second input's distance from base is less than prev distance.
-        if(isGreaterThanDistance(x2, y2)) {
+        if(Math.sqrt(Math.pow(x2 - X_BASE, 2) + Math.pow(y2 - Y_BASE, 2)) < distance) {
             nearest_x = x2;
             nearest_y = y2;
         }
@@ -46,35 +44,12 @@ public class Origin {
         int y3 = scan.nextInt();
 
         // Using math equation to calculate if third input's distance from base is less than prev distance.
-        if(isGreaterThanDistance(x3, y3)) {
+        if(Math.sqrt(Math.pow(x3 - X_BASE, 2) + Math.pow(y3 - Y_BASE, 2)) < distance) {
             nearest_x = x3;
             nearest_y = y3;
         }
 
         // Output the nearest x & y.
         System.out.printf("The nearest point to the origin is (%s,%s)\n", nearest_x, nearest_y);
-    }
-
-    /**
-     * This method performs a math calculation of calculating a distance between two points on a graph.
-     *
-     * @param x a point on the x-axis
-     * @param y a point on the y-axis
-     * @return double
-     */
-    private static double getDistance(int x, int y) {
-        return Math.sqrt(Math.pow(x - X_BASE, 2) + Math.pow(y - Y_BASE, 2));
-    }
-
-    /**
-     * This method performs a math calculation and logically check if the calculation's
-     * result is greater than the local private variable of distance.
-     *
-     * @param x a point on the x-axis
-     * @param y a point on the y-axis
-     * @return a boolean
-     */
-    private static boolean isGreaterThanDistance(int x, int y) {
-        return getDistance(x, y) < _distance;
     }
 }
